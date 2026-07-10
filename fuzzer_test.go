47402b11f9a2388ad548e5593730b5ff0ae5f679
OLEG <oleg@powerc.ru>
2026-07-10 17:53:43 +0300

    test files moved to folder

--- fuzzer_test.go
+++ fuzzer_test.go
@@ -1,14 +1,13 @@
-package tests
+package PowerSNMPv3
 
 import (
 	"testing"
 
 	ASNber "github.com/OlegPowerC/asn1modsnmp"
-	"github.com/OlegPowerC/powersnmpv3"
 )
 
-func fuzzSessionV2() *PowerSNMPv3.SNMPv3Session {
-	s := &PowerSNMPv3.SNMPv3Session{}
+func fuzzSessionV2() *SNMPv3Session {
+	s := &SNMPv3Session{}
 	s.SNMPparams.SNMPversion = 2
 	s.SNMPparams.Community = "fuzzcommunity"
 	return s
@@ -20,28 +19,28 @@ func FuzzReceiverV2(f *testing.F) {
 	oidSys := []int{1, 3, 6, 1, 2, 1, 1, 1, 0}
 	oidIf := []int{1, 3, 6, 1, 2, 1, 2, 2, 1, 2, 1}
 
-	nullVB := []PowerSNMPv3.SNMP_Packet_V2_VarBind{{oidSys, ASNber.NullRawValue}}
-	multiVB := []PowerSNMPv3.SNMP_Packet_V2_VarBind{
+	nullVB := []SNMP_Packet_V2_VarBind{{oidSys, ASNber.NullRawValue}}
+	multiVB := []SNMP_Packet_V2_VarBind{
 		{oidSys, ASNber.NullRawValue},
-		{oidIf, PowerSNMPv3.Convert_setvar_toasn1raw(PowerSNMPv3.SetSNMPVar_OctetString("fuzz-seed"))},
+		{oidIf, Convert_setvar_toasn1raw(SetSNMPVar_OctetString("fuzz-seed"))},
 	}
 	// Exception-varbind noSuchObject: context-specific, tag 0, пустое значение
-	excVB := []PowerSNMPv3.SNMP_Packet_V2_VarBind{{oidSys,
-		ASNber.RawValue{Class: ASNber.ClassContextSpecific, Tag: PowerSNMPv3.tagERR_noSuchObject}}}
+	excVB := []SNMP_Packet_V2_VarBind{{oidSys,
+		ASNber.RawValue{Class: ASNber.ClassContextSpecific, Tag: tagERR_noSuchObject}}}
 
 	seeds := []struct {
-		vbs        []PowerSNMPv3.SNMP_Packet_V2_VarBind
+		vbs        []SNMP_Packet_V2_VarBind
 		reqType    int
 		nr, maxRep int32
 	}{
-		{nullVB, PowerSNMPv3.SNMPv2_REQUEST_GET, 0, 0},
-		{nullVB, PowerSNMPv3.SNMPv2_REQUEST_GETNEXT, 0, 0},
-		{multiVB, PowerSNMPv3.SNMPv2_REQUEST_RESPONSE, 0, 0},
-		{multiVB, PowerSNMPv3.SNMPv2_REQUEST_SET, 0, 0},
-		{nullVB, PowerSNMPv3.SNMPv2_REQUEST_GETBULK, 0, 25},
+		{nullVB, SNMPv2_REQUEST_GET, 0, 0},
+		{nullVB, SNMPv2_REQUEST_GETNEXT, 0, 0},
+		{multiVB, SNMPv2_REQUEST_RESPONSE, 0, 0},
+		{multiVB, SNMPv2_REQUEST_SET, 0, 0},
+		{nullVB, SNMPv2_REQUEST_GETBULK, 0, 25},
 		{multiVB, 6, 0, 0}, // InformRequest
 		{multiVB, 7, 0, 0}, // SNMPv2-Trap
-		{excVB, PowerSNMPv3.SNMPv2_REQUEST_RESPONSE, 0, 0},
+		{excVB, SNMPv2_REQUEST_RESPONSE, 0, 0},
 	}
 	for _, sd := range seeds {
 		pkt, err := s.makeSNMPPv2Packet(sd.vbs, 12345, sd.reqType, sd.nr, sd.maxRep)
@@ -58,11 +57,11 @@ func FuzzReceiverV2(f *testing.F) {
 	f.Fuzz(func(t *testing.T, data []byte) {
 		sess := fuzzSessionV2()
 
-		var vs PowerSNMPv3.SNMP_Packet_V2
+		var vs SNMP_Packet_V2
 		_, _ = ASNber.Unmarshal(data, &vs)
 
 		_, _ = sess.receiverV2parser(data, false, 0)
 		_, _ = sess.receiverV2parser(data, true, 12345)
-		_, _, _, _ = PowerSNMPv3.ParseTrapUsername(data)
+		_, _, _, _ = ParseTrapUsername(data)
 	})
 }
